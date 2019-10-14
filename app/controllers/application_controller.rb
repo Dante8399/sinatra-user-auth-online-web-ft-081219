4f469f0a80b80f3906a8306f12a6f2f6b4ba8130
Dante Spencer <spencerdantel@gmail.com>
2019-10-14 12:37:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
     @user.save
     session[:user_id] = @user.id
 
-    redirect '/users/home'
+    redirect '/recipes'
   end
 
   get '/sessions/login' do
